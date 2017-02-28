926b033bb714b76b7608a1ae83c04293650767b3
gerry <gerry.qi@outlook.com>
2017-02-28 11:45:36 +0800

    init

---
+++ src/main/java/com/ilogic/InputController.java
@@ -0,0 +1,15 @@
+package com.ilogic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by gerry on 2017/2/28.
+ */
+@RestController
+public class InputController {
+    @GetMapping("/")
+    public String index() {
+        return "你好,docker";
+    }
+}
